ee79b94efc0beadd8904227ab5f04718ea6729cd
Miroslav I <styylzeh@gmail.com>
2022-01-24 16:11:09 +0200

    feat(6): connected to MongoDB Atlass

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose')
 require('dotenv').config();
 const fruitRouter = require('./routes/fruit-router')
 
 const server = express();
-const {SERVER_PORT} = process.env;
+const {SERVER_PORT, DB_CONNECTION} = process.env;
 
 //Middlewares
 server.use(morgan('tiny'))
@@ -15,7 +16,17 @@ server.use(express.json())
 //Response handlers
 //Kuomet url prasideda '/api/fruits' naudok fruitRouteri
 server.use('/api/fruits',fruitRouter)
+ mongoose.connect(DB_CONNECTION)
 
  server.listen(SERVER_PORT, ()=>{
+   (async () => {
+   try {
+     await mongoose.connect(DB_CONNECTION)
+     console.error('Pavyko prisijungti')
+
+   } catch (error) {
+     console.error('nepavyko prisijungti')
+   }
    console.log(`puslapis veikia ant http://localhost:${SERVER_PORT}`);
- })
+ })();
+ });
