56dc6970d9869274af3e20e247d8e9896c2bba5e
Miroslav I <styylzeh@gmail.com>
2022-01-26 13:32:31 +0200

    fix(8): reverted tooltip

--- client/src/components/partials/navbar/index.jsx
+++ client/src/components/partials/navbar/index.jsx
@@ -115,6 +115,11 @@ const Navbar = () => {
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
             <Menu
               sx={{ mt: '45px' }}
               id="menu-appbar"
