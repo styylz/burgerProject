2d9ce76a8477aa9fca6e2d2e3db093bd1c493338
Miroslav I <styylzeh@gmail.com>
2022-01-26 09:21:15 +0200

    fix(7): deleted jwt require

--- server/src/controllers/user-controller.js
+++ server/src/controllers/user-controller.js
@@ -6,7 +6,6 @@ const getUsers = async(req,res) => {
   const userDocs = await UserModel.find()
   const users = userDocs.map(userDoc => new UserViewModel(userDoc))
   res.status(200).json({users})
-
 }
 
 module.exports = {
